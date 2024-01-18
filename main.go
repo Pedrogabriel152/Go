6257e490171e39b3b331fcc789f726dda4515c9c
Pedro Gabriel <pedrp@gmail.com>
2024-01-18 13:40:36 -0300

    Começando em GO

--- main.go
+++ main.go
@@ -2,19 +2,55 @@ package main
 
 import "fmt"
 
+type Pessoa struct {
+	Nome      string
+	Sobrenome string
+	Idade     uint8
+	Status    bool
+}
+
+func (p Pessoa) String() string {
+	return fmt.Sprintf("Olá, meu nome é %s e eu tenho %d ano.", p.Nome, p.Idade)
+}
+
+type Categoria struct {
+	Nome string
+	Pai  *Categoria
+}
+
+func (c Categoria) HasParent() bool {
+	return c.Pai != nil
+}
+
+func (c *Categoria) SetPai(pai *Categoria) {
+	c.Pai = pai
+}
+
 func main() {
-	nomes := []string{"Pedro", "Daniel", "Thiago"}
-	var i int
-	for i := 0; i < len(nomes); i++ {
-		fmt.Println(nomes[i])
+	idades := make(map[string]uint8)
+
+	idades["Pedro"] = 21
+	idades["Gabi"] = 19
+
+	if val, ok := idades["Pedro"]; ok {
+		fmt.Println(val, ok)
 	}
 
-	for _, nome := range nomes {
-		fmt.Println(nome)
+	p := Pessoa{
+		Nome:      "Pedro",
+		Sobrenome: "Gabriel",
+		Idade:     21,
+		Status:    true,
 	}
 
-	for i < len(nomes) {
-		fmt.Println(nomes[i])
-		i++
+	cat := Categoria{
+		Nome: "Categoria 1",
 	}
+	cat.SetPai(&Categoria{Nome: "Pai"})
+
+	if !cat.HasParent() {
+		fmt.Println("Não tem pai")
+	}
+
+	fmt.Println(p)
 }
