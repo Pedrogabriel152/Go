55d42cd81332c9bf6b9f24fef32ab097adba1a10
Pedro Gabriel <pedrp@gmail.com>
2024-01-18 15:33:49 -0300

    Começando em GO

--- main.go
+++ main.go
@@ -2,54 +2,86 @@ package main
 
 import "fmt"
 
+// type Pessoa struct {
+// 	Nome      string
+// 	Sobrenome string
+// 	Idade     uint8
+// 	Status    bool
+// }
+
+// func (p Pessoa) String() string {
+// 	return fmt.Sprintf("Olá, meu nome é %s e eu tenho %d ano.", p.Nome, p.Idade)
+// }
+
+// type Categoria struct {
+// 	Nome string
+// 	Pai  *Categoria
+// }
+
+// func (c Categoria) HasParent() bool {
+// 	return c.Pai != nil
+// }
+
+// func (c *Categoria) SetPai(pai *Categoria) {
+// 	c.Pai = pai
+// }
+
 type Pessoa struct {
-	Nome      string
+	Nome   string
+	Idade  int
+	Status bool
+}
+
+type PessoaFisica struct {
+	Pessoa
 	Sobrenome string
-	Idade     uint8
-	Status    bool
+	cpf       string
 }
 
-func (p Pessoa) String() string {
-	return fmt.Sprintf("Olá, meu nome é %s e eu tenho %d ano.", p.Nome, p.Idade)
+type PessoaJuridica struct {
+	Pessoa
+	RazaoSocial string
+	Cnpj        string
 }
 
-type Categoria struct {
-	Nome string
-	Pai  *Categoria
-}
-
-func (c Categoria) HasParent() bool {
-	return c.Pai != nil
-}
-
-func (c *Categoria) SetPai(pai *Categoria) {
-	c.Pai = pai
+func (p PessoaFisica) String() string {
+	return fmt.Sprintf("Olá meu nome é %s e eu tenho %d anos.", p.Nome, p.Idade)
 }
 
 func main() {
-	idades := make(map[string]uint8)
+	// idades := make(map[string]uint8)
 
-	idades["Pedro"] = 21
-	idades["Gabi"] = 19
+	// idades["Pedro"] = 21
+	// idades["Gabi"] = 19
 
-	if val, ok := idades["Pedro"]; ok {
-		fmt.Println(val, ok)
-	}
+	// if val, ok := idades["Pedro"]; ok {
+	// 	fmt.Println(val, ok)
+	// }
 
-	p := Pessoa{
-		Nome:      "Pedro",
-		Sobrenome: "Gabriel",
-		Idade:     21,
-		Status:    true,
-	}
+	// p := Pessoa{
+	// 	Nome:      "Pedro",
+	// 	Sobrenome: "Gabriel",
+	// 	Idade:     21,
+	// 	Status:    true,
+	// }
 
-	cat := Categoria{
-		Nome: "Categoria 1",
-	}
-	cat.SetPai(&Categoria{Nome: "Pai"})
+	// cat := Categoria{
+	// 	Nome: "Categoria 1",
+	// }
+	// cat.SetPai(&Categoria{Nome: "Pai"})
 
-	if !cat.HasParent() {
-		fmt.Println("Não tem pai")
+	// if !cat.HasParent() {
+	// 	fmt.Println("Não tem pai")
+	// }
+
+	// fmt.Println(p)
+
+	p := PessoaFisica{
+		Pessoa{
+			Nome:   "Pedro",
+			Idade:  21,
+			Status: true,
+		}, "Gabriel", "70683499190",
 	}
 
 	fmt.Println(p)
